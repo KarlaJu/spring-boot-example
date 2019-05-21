ce8697c401cdd554a7e6b19d5f814acc450eb6d0
Karla <karla@makingdevs.com>
2019-05-21 11:49:54 -0500

    Mapeando la primera vista con thymeleaf

--- src/main/groovy/mx/edu/ebc/comision/maestria/controllers/ExampleController.groovy
+++ src/main/groovy/mx/edu/ebc/comision/maestria/controllers/ExampleController.groovy
@@ -1,1 +1,15 @@
-package mx.edu.ebc.comision.controllers
+package mx.edu.ebc.comision.maestria.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class ExampleController {
+  @RequestMapping("/")
+  @ResponseBody
+  public ModelAndView home() {
+    ModelAndView model = new ModelAndView("postgrado/home")
+  }
+}
