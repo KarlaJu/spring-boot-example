e623f014b69a83abde7070e0de0e857b1dd70edc
Karla <karla@makingdevs.com>
2019-05-22 15:41:37 -0500

    Prueba de concepto vue de las x-templates

---
+++ src/main/resources/static/js/home/components/searchComponent.js
@@ -0,0 +1,12 @@
+Vue.component('my-checkbox', {
+	template: '#checkbox-template',
+	data() {
+		return { checked: false, title: 'Check me' }
+	},
+	methods: {
+    check() { this.checked = !this.checked; },
+    clickButton: function(){
+      console.log("its work!!")
+    }
+	}
+});
---
+++ src/main/resources/static/js/home/index.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+  el: '#app',
+  data: {},
+  created: function(){
+    console.log("Creando instancia")
+  },
+  methods: {
+     
+  }
+})
